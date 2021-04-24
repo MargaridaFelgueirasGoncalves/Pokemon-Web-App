15bca4bcb7a791420662ae55b1a599dd7c9e0df4
margaridafelgueirasgoncalves <margaridafelgueirasgoncalves@gmail.com>
2021-04-24 15:12:27 +0100

    fixed images size

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,58 @@
 //import Head from 'next/head'
 //import styles from '../styles/Home.module.css'
 
+import { ta } from "date-fns/locale";
 import { api } from "../services/api";
+import styles from './home.module.scss';
+import Image from 'next/image';
+import Link from 'next/link';
 
-
-export default function Home(props) {
+export default function Home({allCards}) {
   return (
-    <div>
-    <h1>Cenas</h1>
-    <p>{JSON.stringify(props.cards)}</p>
+    <div className={styles.homepage}>
+      <section className={styles.allCards}>
+        <table  className="bordered striped centered highlight responsive-table">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Type</th>
+              <th>Release Date</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {allCards.map(card => {
+              return (
+                <tr key={card.id}>
+                  <td>
+                  <Image
+                    width={120}
+                    height={120}
+                    src={card.image}
+                    alt={card.name}
+                    objectFit="cover"
+                  />
+                  </td>
+                  <td>
+                    <Link href={'/card/${card.name}'}>
+                      <a>{card.name}</a>
+                    </Link>
+                  </td>
+                  <td>
+                  <Link href={'/card/${card.supertype}'}>
+                      <a>{card.supertype}</a>
+                    </Link>
+                  </td>
+                  <td>
+                    <a href="">{card.releaseDate}</a>
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+    </section>
     </div>
   )
 }
@@ -18,14 +62,19 @@ export async function getStaticProps() {
  
   const cards = data.data.map(card => {
     return {
+      id: card.id,
       name: card.name,
       image: card.images.small,
+      supertype: card.types,
+      releaseDate: card.set.releaseDate,
     };
   })
+
+  const allCards= cards;
   
   return {
     props: {
-      cards: data,
+      allCards,
     },
 
     revalidate: 60*60*8,
