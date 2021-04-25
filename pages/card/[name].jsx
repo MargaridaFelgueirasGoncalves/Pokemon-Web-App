8a24618b9d92c0031833fbd124a7f93658320c2d
margaridafelgueirasgoncalves <margaridafelgueirasgoncalves@gmail.com>
2021-04-25 16:23:40 +0100

    added card properties

--- pages/card/[name].jsx
+++ pages/card/[name].jsx
@@ -25,52 +25,100 @@ export async function getStaticProps(ctx) {
     const {name} =ctx.params;
     
 
-   const {data} = await api.get('cards')
-    let cenas = data.data[0];
+   const {data} = await api.get('cards?q=name:'+name)
+    let pokemon = data.data[0];
 
     const card = {
-        id:cenas.id,
-        name: cenas.name,
-        image: cenas.images.large,
-        type: cenas.types,
-        releaseDate: cenas.set.releaseDate,
-    }
+        id:pokemon.id,
+        name: pokemon.name,
+        level: verifyInformation(pokemon.level),
+        image: pokemon.images.large,
+        type: listTypes(pokemon.types),
+        evolvesTo: verifyInformation(pokemon.evolvesTo),
+        evolvesFrom: verifyInformation(pokemon.evolvesFrom),
+        weaknesses: {
+            type: verifyInformation(pokemon.weaknesses.type),
+            value: verifyInformation(pokemon.weaknesses.value),
+        },
+        attacks: mapAttacks(pokemon.attacks),
+        releaseDate: pokemon.set.releaseDate,
+        hp: verifyInformation(pokemon.hp),
+        setName: verifyInformation(pokemon.set.name),
+        series: verifyInformation(pokemon.series),
+        resistances: {
+            type: verifyInformation(pokemon.resistances.type),
+            value: verifyInformation(pokemon.resistances.value),
+        },
+        convertedRetreatCost: verifyInformation(pokemon.convertedRetreatCost),
+        //abilities: mapAbilities(pokemon.abilities),
+    };
    
-
     return {
         props: {
             card,
         },
 
         revalidate: 60*60*24,
+
     }
 }
 
+    function verifyInformation (information) {
+        if (information === undefined) {
+            return "Nothing to show here!";
+        }
+
+        return information;
+    }
 
 
+    function mapAttacks (information) {
+        if (information === undefined) {
+            return "Nothing to show here!";
+        }
 
-    /*const card = {
-        id: data.id,
-      name: data.name,
-      image: data.images.large,
-      type: data.types,
-      releaseDate: data.data.set.releaseDate,
-      level: data.data.level,
-      abilityName: data.data.abilities.name,
-      abilityDescription: data.data.abilities.text,
-      abilityType: data.data.abilities.type,
-      evolvesTo: data.evolvesTo,
-      evolvesFrom: data.evolvesFrom,
-      attackName: data.attacks[0].name,
-      attackCost: data.attacks[0].cost,
-      attackConvertedEnergyCost: data.attacks[0].convertedEnergyCost,
-      attackDamage: data.attacks[0].damage,
-      attackDescription: data.attacks[0].text,
-      weaknessesType: data.weaknesses.type,
-      weaknessesValue: data.weaknesses.value,
-      resistancesType: data.resistances.type,
-      resistancesValue: data.resistances.value,
-      } */
+        information.map(attack => {
     
+            return {
+              name: verifyInformation(attack.name),
+              convertedEnergyCost: verifyInformation(attack.convertedEnergyCost),
+              damage: verifyInformation(attack.damage),
+              description: verifyInformation(attack.text),
+              /*cost: function listCosts() {
+                  let costs = "";
+                  for(let i= 0; i<attack.cost.length; i++) {
+                    costs=costs + attack.cost[i];
+                  }
+                  return costs;
+              }*/
+            }
+         })
+    }
 
-  
+    function listTypes (information) {
+        if (information === undefined) {
+            return "Nothing to show here!";
+        }
+
+        let types = "";
+        for(let i= 0; i<information.length; i++) {
+          types=types + information[i];
+        }
+        return types;
+    }
+
+    function mapAbilities (information) {
+        if (information === undefined) {
+            return "Nothing to show here!";
+        }
+
+        information.map(ability => {
+    
+            return {
+              name: ability.name,
+              type: ability.type,
+              description: ability.text,
+
+            };
+         })
+    }
